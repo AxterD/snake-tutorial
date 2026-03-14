3582cb9fda9fcd8a2d4c119e95214d633e0bde88
Axter Debringston <AxterD>
2026-03-15 00:11:32 +0100

    Lesson 3 progress

--- noki/snake_game/src/snake_game.adb
+++ noki/snake_game/src/snake_game.adb
@@ -1,6 +1,25 @@
 with Noki;
 
 procedure Snake_Game is
+type GameState_T is (Welcome, Play, Game_Over, Undefined);
+Game_State : GameState_T := Welcome;
+
 begin
-   Noki.Log ("Welcometo my Snake Game!");
+loop
+      Noki.Log(Noki.Clear_Screen);
+    case Game_State is
+        when Welcome =>
+            Noki.Log("Welcome to Sankotron!");
+            Game_State := Play;
+        when Play =>
+            Noki.Log("We are Playing!");
+            Game_State := Game_Over;
+        when Game_Over =>
+            Noki.Log("Game Over!");
+            Game_State := Undefined;
+        when others =>
+            Noki.Log("Press Ctrl+C to abort game");
+    end case;
+    delay 1.0;
+end loop;
 end Snake_Game;
