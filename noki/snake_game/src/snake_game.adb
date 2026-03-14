ce16c8a80096cbbe554eafb37a897fc1deafb4db
Axter Debringston <AxterD>
2026-03-15 00:33:07 +0100

    Lesson 4 progress

--- noki/snake_game/src/snake_game.adb
+++ noki/snake_game/src/snake_game.adb
@@ -1,25 +1,40 @@
+with GNAT.OS_Lib;
+
 with Noki;
 
 procedure Snake_Game is
+package N renames Noki;
+use N;
 type GameState_T is (Welcome, Play, Game_Over, Undefined);
 Game_State : GameState_T := Welcome;
+N_Input_Task : Input_Task_T;
 
 begin
 loop
-      Noki.Log(Noki.Clear_Screen);
+    exit when Input_Cmd.Get = Quit;
+    Log(Clear_Screen);
     case Game_State is
         when Welcome =>
-            Noki.Log("Welcome to Sankotron!");
-            Game_State := Play;
+            Log("Welcome to Sankotron!");
+            Log ("Press Enter/Space to play, Q/Esc to quit");
+            if Input_Cmd.Get = Enter then
+               Game_State := Play;
+               Input_Cmd.Reset;
+            end if;
         when Play =>
-            Noki.Log("We are Playing!");
+            Log("We are Playing!");
             Game_State := Game_Over;
         when Game_Over =>
-            Noki.Log("Game Over!");
-            Game_State := Undefined;
+            Log ("Game Over!");
+            Log ("Press Enter/Space to replay");
+            if Input_Cmd.Get = Enter then
+               Game_State := Play;
+               Input_Cmd.Reset;
+            end if;
         when others =>
-            Noki.Log("Press Ctrl+C to abort game");
+            Log("Press Ctrl+C to abort game");
     end case;
     delay 1.0;
 end loop;
+GNAT.OS_Lib.OS_Exit (0);
 end Snake_Game;
